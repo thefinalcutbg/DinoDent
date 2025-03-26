ee1b8bb56f10d8303b64f7db80a319b356112144
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2025-03-26 16:04:19 +0200

    open directory after saving as pdf fix

--- src/Presenter/FinancialPresenter.cpp
+++ src/Presenter/FinancialPresenter.cpp
@@ -329,7 +329,7 @@ void FinancialPresenter::print()
     if (save())
     Print::invoice(m_invoice);
 }
-
+#include <QDebug>
 void FinancialPresenter::pdfPrint()
 {
     if (!save()) return;
@@ -343,7 +343,8 @@ void FinancialPresenter::pdfPrint()
     if (ModalDialogBuilder::askDialog(
         "Файлът е запазен успешно. Желаете ли да отворите директорията?"
     )) {
-        ModalDialogBuilder::openExplorer(filepath);
+        ModalDialogBuilder::openExplorer(filepath + ".pdf");
+        qDebug() << filepath;
     }
 
 }

166e8404fbac9144580935f0413af603877f5e93
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2026-01-06 15:15:22 +0200

    Update PlannedProcedureDialog.cpp

--- src/View/Widgets/PlannedProcedureDialog.cpp
+++ src/View/Widgets/PlannedProcedureDialog.cpp
@@ -10,7 +10,11 @@ PlannedProcedureDialog::PlannedProcedureDialog(TreatmentPlan::PlannedProcedure& 
 
     setWindowTitle("Редактиране на процедура");
 
-    ui->nameEdit->setText(p.getNameText().c_str());
+    ui->nameEdit->setPlaceholderText(p.code.name().c_str());
+
+    if(p.name.size()){
+        ui->nameEdit->setText(p.name.c_str());
+    }
 
     ui->priceInput->setPrice(p.priceRange);
 

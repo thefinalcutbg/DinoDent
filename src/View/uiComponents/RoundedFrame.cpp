e5f99f76126c34642f407b29ca60b454344dc727
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2026-01-09 20:57:23 +0200

    fix paint when shadow is removed

--- src/View/uiComponents/RoundedFrame.cpp
+++ src/View/uiComponents/RoundedFrame.cpp
@@ -8,8 +8,7 @@ RoundedFrame::RoundedFrame(QWidget* parent)
     : QFrame(parent)
 {
     m_color = Theme::mainBackgroundColor;
-    setAttribute(Qt::WA_TranslucentBackground, true);
-    setAutoFillBackground(false);
+
     Theme::applyShadow(this);
 }
 
@@ -35,11 +34,10 @@ void RoundedFrame::paintEvent(QPaintEvent* event)
 
     QPainter p(this);
     p.setRenderHint(QPainter::Antialiasing, true);
-    p.setRenderHint(QPainter::SmoothPixmapTransform, true);
 
-    p.setCompositionMode(QPainter::CompositionMode_Source);
-    p.fillRect(rect(), Qt::transparent);
-    p.setCompositionMode(QPainter::CompositionMode_SourceOver);
+    if (!graphicsEffect()) {
+        p.fillRect(rect(), Theme::background);
+    }
 
     QPen pen(m_hasChildFocus ? m_color : Theme::border);
     pen.setCosmetic(true);
@@ -47,20 +45,17 @@ void RoundedFrame::paintEvent(QPaintEvent* event)
     p.setPen(pen);
 
     const qreal half = pen.widthF() * 0.5;
-
     QRectF r = rect();
     r.adjust(half, half, -half, -half);
 
     QPainterPath path;
-    path.addRoundedRect(r, Theme::radius/2, Theme::radius/2);
+    path.addRoundedRect(r, Theme::radius / 2, Theme::radius / 2);
 
     p.fillPath(path, Theme::sectionBackground);
-
     p.drawPath(path);
 
-    if (m_separatorPosition != -1) {
+    if (m_separatorPosition != -1)
         p.drawLine(m_separatorPosition, 0, m_separatorPosition, height());
-    }
 }
 
 void RoundedFrame::updateFocusState()
